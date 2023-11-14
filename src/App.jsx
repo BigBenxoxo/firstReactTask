f9ba7481653129711926ee23ce8076f7d791d9fa
BenPet <bendzuke@gmail.com>
2023-11-14 12:55:33 +0200

    added search

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,22 @@ function Greeting({ name }) {
 
 function App() {
   const [persons, setPersons] = useState([
-    //Handling event + modify state
+    //Modify state
     { id: 1, name: "Bendik", title: "CEO", age: 31 },
     { id: 2, name: "John", title: "Cleaner", age: 40 },
     { id: 3, name: "Alice", title: "Receptionist", age: 25 },
   ]);
+  const [search, setSearch] = useState("");
 
   const removeHandler = (id) => {
     // Handling event
     const updatedArray = persons.filter((person) => person.id !== id);
     setPersons(updatedArray);
+  };
+
+  const searchHandler = (e) => {
+    // Why is this = (e)
+    setSearch(e.target.value);
   };
 
   return (
@@ -31,15 +37,21 @@ function App() {
       <main>
         <Greeting name="Someone"></Greeting>
         <img src={Logo} />
+        <input type="text" onChange={searchHandler} />
+
         <h2>This is my application</h2>
         <div className="cards">
-          {persons.map((person) => (
-            <Card
-              key={person.id}
-              {...person}
-              click={() => removeHandler(person.id)} //Event function, that we pass to the child in Card.jsx (onClick)
-            />
-          ))}
+          {persons
+            .filter((person) =>
+              person.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((person) => (
+              <Card
+                key={person.id}
+                {...person}
+                click={() => removeHandler(person.id)} //Event function, that we pass to the child in Card.jsx (onClick)
+              />
+            ))}
         </div>
         <button className="btn">Click me</button>
       </main>
